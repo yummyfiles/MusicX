52bd33894534fdd17e2712deb379d394ce259ae8
YUMMYFILES // DEV <yummyfiles@dev.com>
2026-08-23 12:49:08 -0400

    Final script test: android.sdk.directory property bypass

--- app/src/main/java/com/yummyfiles/musicx/ui/settings/AboutScreen.kt
+++ app/src/main/java/com/yummyfiles/musicx/ui/settings/AboutScreen.kt
@@ -26,7 +26,7 @@ import com.yummyfiles.musicx.ui.theme.MusicXTheme
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AboutScreen(onBack: () -> Unit) {
-    // bruh this app is literally built different lol
+    // bruh this app is literally built different fr lol
     Scaffold(
         topBar = {
             TopAppBar(
