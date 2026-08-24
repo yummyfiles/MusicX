11aea59be65dee089ff06ab59b0b7f850cb5c95e
YUMMYFILES // DEV <yummyfiles@dev.com>
2026-08-24 17:20:39 -0400

    chore: bump version to v2.0.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.yummyfiles.musicx"
         minSdk = 24
         targetSdk = 37
-        versionCode = 8
-        versionName = "2.0.7"
+        versionCode = 9
+        versionName = "2.0.8"
     }
 
     signingConfigs {
